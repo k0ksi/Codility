75bda3041cfcc4b2fba6c45e099a07a759c2c847
Zhelyazko Stoyanov <zh.stoqnov@gmail.com>
2018-01-14 14:11:15 +0200

    Codility MinAbsSumOfTwo solved - catterpillar

---
+++ MinAbsSumOfTwo/MinAbsSumOfTwo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MinAbsSumOfTwo
+{
+    class MinAbsSumOfTwo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().solution(new int[] { -8, 4, 5, -10, 3 }));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int n = A.Length;
+            Array.Sort(A);
+            int tail = 0;
+            int head = n - 1;
+            int minAbsSum = Math.Abs(A[tail] + A[head]);
+            while (tail <= head)
+            {
+                int currentSum = A[tail] + A[head];
+                minAbsSum = Math.Min(minAbsSum, Math.Abs(currentSum));
+
+                if(currentSum <= 0)
+                {
+                    tail++;
+                }
+                else
+                {
+                    head--;
+                }
+            }
+
+            return minAbsSum;
+        }
+    }
+}
